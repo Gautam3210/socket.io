a64cbb85875d8d1b2d99bf2705b608db1e042fec
Gautam Malviya <gmalviya619@gmail.com>
2025-05-05 00:09:16 +0530

    initial commit

---
+++ server/app.js
@@ -0,0 +1,11 @@
+const express = require('express')
+const http = require('http')
+const app = express();
+
+const server = http.createServer(app)
+
+
+
+server.listen(5000, ()=>{
+  console.log('server is running....')
+})
