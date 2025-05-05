bfc27a2e5d6b67ab61abe711340501cdd094f4f7
Gautam Malviya <gmalviya619@gmail.com>
2025-05-05 23:28:37 +0530

    adding frontend

---
+++ client/src/App.jsx
@@ -0,0 +1,59 @@
+import "bootstrap/dist/css/bootstrap.min.css";
+import { useEffect, useRef } from "react";
+import { connect, io } from "socket.io-client";
+
+import "./App.css";
+
+const socket = io("http://localhost:5000");
+
+function App() {
+  useEffect(() => {
+    socket.on("connect", () => {
+      console.log("connected to a server");
+    });
+
+    socket.on("userMessage", (data) => {
+      console.log("Message from server:", data);
+    });
+
+    return () => {
+      socket.off("connect");
+      socket.off("message");
+    };
+  }, []);
+
+  const inputRef = useRef();
+  const handleKeyDown = () => {
+    socket.emit("userMessage", inputRef.current.value);
+    inputRef.current.value = "";
+  };
+  return (
+    <div>
+      <div class="chats">
+        <ul>
+          <li>itms</li>
+        </ul>
+      </div>
+      <footer class="textArea">
+        <form style={{ display: "flex", columnGap: "1rem", height: "3rem" }}>
+          <div style={{ display: "flex", margin: "0rem", width: "20rem" }}>
+            <label for="exampleInputEmail1" class="form-label"></label>
+            <input
+              ref={inputRef}
+              type="text"
+              class="form-control"
+              id="exampleInputEmail1"
+              aria-describedby="emailHelp"
+            />
+          </div>
+
+          <button type="submit" class="btn btn-primary" onSubmit={handleKeyDown}>
+            send
+          </button>
+        </form>
+      </footer>
+    </div>
+  );
+}
+
+export default App;
--- server/app.js
+++ server/app.js
@@ -1,10 +1,37 @@
+
 const express = require('express')
+const cors = require('cors')
+const { Server } = require("socket.io");
 const http = require('http')
 const app = express();
 
+
 const server = http.createServer(app)
 
+const io = new Server(server,{
+  cors: {
+    origin: "http://localhost:5173", // your React app origin
+    methods: ["GET", "POST"]
+  }
+});
 
+app.use(cors())
+
+app.get('/', (req, res)=>{
+  res.send('hello world')
+})
+
+io.on('connection', (socket) => {
+  console.log('a user connected');
+  socket.on('userMessage', (msg)=>{
+    console.log(socket.id)
+    console.log('message : '+ msg)
+    io.emit('userMessage',msg)
+  })
+});
+
+// socket.on('userMessage', (msg)=>{
+//   console.log('message : '+ msg)})
 
 server.listen(5000, ()=>{
   console.log('server is running....')
